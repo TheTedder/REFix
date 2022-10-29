e96ac1b6a12ea7ed3751bb57794c96f589c665f7
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2022-10-28 22:18:54 -0400

    Get the player camera controller.

--- REFix.cpp
+++ REFix.cpp
@@ -8,7 +8,17 @@ extern "C" __declspec(dllexport) bool reframework_plugin_initialize(const REFram
     const REF::API::TDB* tdb = api->tdb();
     const REF::API::VMContext* context = api->get_vm_context();
 
-    // Plugin functionality goes here.
+    const REF::API::ManagedObject* camera_system = api->get_managed_singleton("app.ropeway.camera.CameraSystem");
+    const REF::API::TypeDefinition* camera_system_type = tdb->find_type("app.ropeway.camera.CameraSystem");
+    const REF::API::Method* get_camera_controller = camera_system_type->find_method("getCameraController");
+    const REF::API::ManagedObject* player_camera_controller = get_camera_controller->call<REF::API::ManagedObject*>(context, camera_system, 0);
+
+    if (player_camera_controller == nullptr) {
+        api->log_warn("[REFix] Call to getCameraController failed.");
+    }
+    else {
+        api->log_info("[REFix] Player Camera Controller found at %#.16llx", player_camera_controller);
+    }
 
     return true;
 }
