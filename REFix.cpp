adba6fc35a10356202eb648c62c80aab7f18e4ac
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2022-10-28 22:18:26 -0400

    Don't use `auto`.

--- REFix.cpp
+++ REFix.cpp
@@ -4,9 +4,9 @@ namespace REF = reframework;
 
 extern "C" __declspec(dllexport) bool reframework_plugin_initialize(const REFrameworkPluginInitializeParam* param) {
     REF::API::initialize(param);
-    auto& api = REF::API::get();
-    const REF::API::TDB* tbd = api->tdb();
     REF::API::VMContext* context = api->get_vm_context();
+    const std::unique_ptr<REF::API>& api = REF::API::get();
+    const REF::API::TDB* tdb = api->tdb();
 
     // Plugin functionality goes here.
 
