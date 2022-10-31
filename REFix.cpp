a14cc6d662e03de31b53f77f6dcd359ab0996045
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2022-10-30 22:23:18 -0400

    Use the TDB's `find_method` function directly.

--- REFix.cpp
+++ REFix.cpp
@@ -11,9 +11,8 @@ extern "C" __declspec(dllexport) bool reframework_plugin_initialize(const REFram
     // Get the player camera controller.
 
     const REF::API::ManagedObject* camera_system = api->get_managed_singleton("app.ropeway.camera.CameraSystem");
-    const REF::API::TypeDefinition* camera_system_type = tdb->find_type("app.ropeway.camera.CameraSystem");
-    const REF::API::Method* get_camera_controller = camera_system_type->find_method("getCameraController");
-    const REF::API::ManagedObject* player_camera_controller = get_camera_controller->call<REF::API::ManagedObject*>(context, camera_system, 0);
+    const REF::API::Method* get_camera_controller = tdb->find_method("app.ropeway.camera.CameraSystem","getCameraController");
+    REF::API::ManagedObject* player_camera_controller = get_camera_controller->call<REF::API::ManagedObject*>(context, camera_system, 0);
 
     if (player_camera_controller == nullptr) {
         api->log_warn("[REFix] Call to getCameraController failed.");
