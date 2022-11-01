5ff2451dbadfc68916ee68747fc5e6ecc7bfd5fe
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2022-10-31 22:22:13 -0400

    Make pointer locals const.

--- REFix.cpp
+++ REFix.cpp
@@ -5,14 +5,14 @@
 bool reframework_plugin_initialize(const REFrameworkPluginInitializeParam* param) {
     REF::API::initialize(param);
     const std::unique_ptr<REF::API>& api = REF::API::get();
-    const REF::API::TDB* tdb = api->tdb();
-    const REF::API::VMContext* context = api->get_vm_context();
+    const REF::API::TDB* const tdb = api->tdb();
+    const REF::API::VMContext* const context = api->get_vm_context();
+    const REF::API::Method* const get_camera_controller = tdb->find_method("app.ropeway.camera.CameraSystem", "getCameraController");
 
     // Get the player camera controller.
 
-    const REF::API::ManagedObject* camera_system = api->get_managed_singleton("app.ropeway.camera.CameraSystem");
-    const REF::API::Method* get_camera_controller = tdb->find_method("app.ropeway.camera.CameraSystem", "getCameraController");
-    const REF::API::ManagedObject* player_camera_controller = get_camera_controller->call<REF::API::ManagedObject*>(context, camera_system, 0);
+    const REF::API::ManagedObject* const camera_system = api->get_managed_singleton("app.ropeway.camera.CameraSystem");
+    const REF::API::ManagedObject* const player_camera_controller = get_camera_controller->call<REF::API::ManagedObject*>(context, camera_system, 0);
 
     if (player_camera_controller == nullptr) {
         api->log_error("[REFix] Call to getCameraController failed.");
@@ -23,7 +23,7 @@ bool reframework_plugin_initialize(const REFrameworkPluginInitializeParam* param
 
     // Get the camera settings.
 
-    const REF::API::ManagedObject* twirler_camera_settings = *player_camera_controller->get_field<REF::API::ManagedObject*>("TwirlerCameraSettings");
+    const REF::API::ManagedObject* const twirler_camera_settings = *player_camera_controller->get_field<REF::API::ManagedObject*>("TwirlerCameraSettings");
     api->log_info("[REFix] Twirler Camera Settings found at %p", twirler_camera_settings);
     return true;
 }
