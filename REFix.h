d1f9f82c933a52f7eb4760c285a7bad319b5b366
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2022-10-31 22:20:30 -0400

    Remove unused windows headers.

--- REFix.h
+++ REFix.h
@@ -1,12 +1,4 @@
 #pragma once
-#include <sdkddkver.h>
-
-#define NTDDI_VERSION NTDDI_WIN10
-#define _WIN32_WINNT _WIN32_WINNT_WIN10
-#define WINVER _WIN32_WINNT_WIN10
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files
-#include <windows.h>
 #include <reframework/API.hpp>
 
 namespace REF = reframework;
