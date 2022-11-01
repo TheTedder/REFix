80dcfcea621b26758e4c0aca18d7c3f163189a46
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2022-10-31 22:20:54 -0400

    Remove unused deref function.

--- REFix.h
+++ REFix.h
@@ -4,8 +4,3 @@
 namespace REF = reframework;
 
 extern "C" __declspec(dllexport) bool reframework_plugin_initialize(const REFrameworkPluginInitializeParam* param);
-
-template <typename T, typename I>
-inline T deref(I* location, int offset) {
-    return  *((T*)(location + offset));
-}
