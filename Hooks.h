f6c98efea930d1d22971b418e29b8d79650d4eec
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2022-11-08 16:34:05 -0500

    Hook `GameRankSystem::addRankPointDirect`.

--- Hooks.h
+++ Hooks.h
@@ -2,6 +2,7 @@
 #include "REFix.h"
 
 namespace REFix {
-    int pre_update_pitch_yaw(int argc, void** argv, REFrameworkTypeDefinitionHandle* arg_tys);
     void post_hook_null(void** ret_val, REFrameworkTypeDefinitionHandle ret_ty);
+    int pre_update_pitch_yaw(int argc, void** argv, REFrameworkTypeDefinitionHandle* arg_tys);
+    int pre_add_rank_point_direct(int argc, void** argv, REFrameworkTypeDefinitionHandle* arg_tys);
 }
