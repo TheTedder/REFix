954452a8dfa34dc9b743a49eda221fbb6eb49c20
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2022-11-27 14:40:04 -0500

    Set the value and not the pointer.

--- Hooks.cpp
+++ Hooks.cpp
@@ -34,7 +34,7 @@ namespace REFix {
     int pre_set_interval_level(int argc, void** argv, REFrameworkTypeDefinitionHandle* arg_tys)
     {
         int32_t* level = (int32_t*)&argv[2];
-        level = 0;
+        *level = 0;
         return REFRAMEWORK_HOOK_CALL_ORIGINAL;
     }
 }
