4babe8641ba145c3a39d01bdf73438d1ac53bc0f
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2023-07-08 16:17:51 -0400

    Specify REFramework version and game name.

---
+++ Version.cpp
@@ -0,0 +1,8 @@
+#include "Version.h"
+
+void reframework_plugin_required_version(REFrameworkPluginVersion* version) {
+    version->major = REFRAMEWORK_PLUGIN_VERSION_MAJOR;
+    version->minor = REFRAMEWORK_PLUGIN_VERSION_MINOR;
+    version->patch = REFRAMEWORK_PLUGIN_VERSION_PATCH;
+    version->game_name = "RE2";
+}
---
+++ Version.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "REFix.h"
+
+extern "C" __declspec(dllexport) void reframework_plugin_required_version(REFrameworkPluginVersion * version);
