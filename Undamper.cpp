5138cdef08a8ce642f5f0f2808d75b363f2abf5d
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2023-08-21 15:54:36 -0400

    Make `Undamper::damping_time` `const`.

--- Undamper.cpp
+++ Undamper.cpp
@@ -1,9 +1,7 @@
 #include "Undamper.h"
 
 namespace REFix {
-    Undamper::Undamper(const REF::API::TypeDefinition* damping_struct_type) {
-        this->damping_time = damping_struct_type->find_field("DampingTime");
-    }
+    Undamper::Undamper(const REF::API::TypeDefinition* damping_struct_type) : damping_time(damping_struct_type->find_field("DampingTime")) {}
 
     void Undamper::undamp(REF::API::ManagedObject* damping_struct) const {
         *(float*)this->damping_time->get_data_raw(damping_struct) = 0.0f;
